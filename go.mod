a70d13b4202184698ded6af62f03ce99b81fc26d
John Bakhmat <johnbakhmat@gmail.com>
2024-01-15 13:32:24 +0300

    .env loading

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module johnbakhmat.tech/pinned
 go 1.21.6
 
 require (
+	github.com/gofiber/fiber/v3 v3.0.0-20240113152607-1588b6b60210
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/gofiber/fiber/v3 v3.0.0-20240113152607-1588b6b60210 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-beta.3 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
--- main.go
+++ main.go
@@ -3,12 +3,29 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    port := 7000
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	portStr := os.Getenv("PORT")
+    if portStr == "" {
+        portStr = "7000"
+    }
+
+    port, err := strconv.Atoi(portStr)
+    if err != nil {
+        log.Fatal(err)
+    }
+
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
